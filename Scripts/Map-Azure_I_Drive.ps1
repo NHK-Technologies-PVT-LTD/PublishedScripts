8f088b4c36eb4787fad6698b5dddfe393358b8fb
imkhan645 <80417130+imkhan645@users.noreply.github.com>
2021-03-16 23:14:30 +0530

    Create Map-Azure_I_Drive.ps1

---
+++ Scripts/Map-Azure_I_Drive.ps1
@@ -0,0 +1,19 @@
+$connectTestResult = Test-NetConnection -ComputerName proravidsa01.file.core.windows.net -Port 445
+$DriveLetter = "I"
+$ReportFile = $DriveLetter + ":\WindowsUpdateData\WindowsUpdateData.csv"
+
+if ($connectTestResult.TcpTestSucceeded) {
+    # Save the password so the drive will persist on reboot
+    cmd.exe /C "cmdkey /add:`"proravidsa01.file.core.windows.net`" /user:`"Azure\proravidsa01`" /pass:`"38qKhh17kWRDKO/q/1Zay4LiLDiUjZDAns4m1bMvbTaNcWSQFLGWixbDTna6sDxCCBtAxg8O+XPrLHWTQEvgbQ==`""
+    # Mount the drive
+    New-PSDrive -Name $DriveLetter -PSProvider FileSystem -Root "\\proravidsa01.file.core.windows.net\reports" -Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
+
+Get-HotFix -ComputerName $ENV:COMPUTERNAME  | Sort-Object -Descending -Property InstalledOn -ErrorAction SilentlyContinue | Select-Object `
+@{Name='Server Name';Expression={$_.PSComputerName}}, `
+@{Name='Company Name';Expression={" "}}, `
+@{Name='Supported By';Expression={" "}}, `
+@{Name='Last Update Date';Expression={$_.InstalledON}}  -First 1 | `
+Export-Csv -Path $ReportFile -Append -NoTypeInformation
